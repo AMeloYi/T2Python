0d46756a6ee2e30319f444110c4ace54f248b6f6
LIYi <ly595788168@hotmail.com>
2017-10-17 18:08:43 +0200

    tp5 final

--- tp5.py
+++ tp5.py
@@ -136,10 +136,56 @@ def ChargerXML():
                     PopulationTotale = subChild.text
             c.execute('INSERT INTO commune VALUES(?, ? ,?, ?)', [CodeCommune, CodeDepartment, NomCommune, PopulationTotale])
     conn.commit()
-    
+
+def AjouterNouvelle():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+
+    c.execute('CREATE TABLE nouvellesregions (CodeRegion int primary key, NomRegion text)')
+    csv_reader = csv.reader(open('zones-2016.csv','r',encoding='utf-8'))
+    count = 0
+    for row in csv_reader:
+        count = count + 1
+        if(count < 4550):
+            continue
+        else:
+            tmp = "".join(row)
+            t, CodeRegion, NomRegion, t = tmp.split(";")
+            c.execute('INSERT INTO nouvellesregions VALUES(?, ?)', [CodeRegion, NomRegion])
+    conn.commit()
+
+    c.execute('ALTER TABLE department ADD CodeNouvelleRegion text')
+    conn.commit()
+
+    csv_reader = csv.reader(open('communes-2016.csv','r',encoding='utf-8'))
+    count = 0
+    temp_CodeDepartment = '0'
+    for row in csv_reader:
+        count = count + 1
+        if(count < 7):
+            continue
+        else:
+            tmp = "".join(row)
+            t, t, CodeDepartment, CodeNouvelleRegion, t, t, t, t= tmp.split(";")
+            if(temp_CodeDepartment == CodeDepartment):
+                continue
+            else:
+                c.execute('UPDATE department SET CodeNouvelleRegion = ? WHERE CodeDepartment = ?', [CodeNouvelleRegion, CodeDepartment])
+                temp_CodeDepartment == CodeDepartment
+    conn.commit()
+
+def Calculer_Nouvelle():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    print('****  les populations totales de chaque région  ****')
+    for row in c.execute('SELECT n.CodeRegion, n.NomRegion, SUM(c.PopulationTotale) FROM commune c ,department d, nouvellesregions n WHERE c.CodeDepartment=d.CodeDepartment AND d.CodeNouvelleRegion=n.CodeRegion GROUP BY n.CodeRegion'):
+        print(row)
+
 if __name__=='__main__':
     #Create_Database()
     #Calculer_Population()
     #Determiner_Communes()
     #SauvegarderBddAXML()
     #ChargerXML()
+    #AjouterNouvelle()
+    #Calculer_Nouvelle()
