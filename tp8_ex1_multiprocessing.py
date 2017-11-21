9cb7afa3e89f3f1b7dbd05c396754f5634f9ce12
LIYi <ly595788168@hotmail.com>
2017-11-21 11:29:55 +0100

    tp8 ex1 ok

--- tp8_ex1_multiprocessing.py
+++ tp8_ex1_multiprocessing.py
@@ -1,18 +1,20 @@
 from multiprocessing import Process
 import time
 
-def print_time(_processName):
+def print_time(processName):
     result = 0
     while result < 100000:
         result += 20
         print("%s : %d" %(processName, result))
 
-# Create two threads as follows
-try:
-    _thread.start_new_thread(print_time, ("Thread-1", ))
-    _thread.start_new_thread(print_time, ("Thread-2", ))
-except:
-    print("Error: unable to start thread")
 
-while 1:
-    pass
+
+
+if __name__ == '__main__':
+    # Create two threads as follows
+    p1 = Process(target=print_time, args=('Thread-1',))
+    p2 = Process(target=print_time, args=('Thread-2',))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
--- tp8_ex1_multithreading.py
+++ tp8_ex1_multithreading.py
@@ -3,16 +3,13 @@ import time
 
 def print_time(threadName):
     result = 0
-    while result < 100000:
+    while result < 500000:
         result += 20
         print("%s : %d" %(threadName, result))
 
-# Create two threads as follows
-try:
+
+if __name__ == '__main__':
+    # Create two threads as follows
     _thread.start_new_thread(print_time, ("Thread-1", ))
     _thread.start_new_thread(print_time, ("Thread-2", ))
-except:
-    print("Error: unable to start thread")
-
-while 1:
-    pass
+    time.sleep(10)
